ba9630cce0a686b03614c8bab6d0b1aa7c6e9db6
Eduardo Farfus <farfus@unochapeco.edu.br>
2025-03-12 19:36:46 -0300

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,7 +10,7 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
+//teste
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
