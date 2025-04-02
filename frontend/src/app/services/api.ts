033696ed29e4e3c7dccf6c0f3ef0df26ae01a46c
thomegabi <gabi10thome@hotmail.com>
2025-04-02 19:41:59 -0300

    complete more

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -2,23 +2,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 export const api = axios.create({
-<<<<<<< HEAD
-  baseURL: 'http://172.20.32.1:3000'
-});
-
-api.interceptors.request.use(
-  async (config) => {
-    const token = await AsyncStorage.getItem('token');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-=======
   baseURL: "http://192.168.11.143:3000",
 });
 
@@ -29,4 +12,3 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
->>>>>>> ccb4d475e7941fb74cf40480efd9a6b8bfba3c2d
